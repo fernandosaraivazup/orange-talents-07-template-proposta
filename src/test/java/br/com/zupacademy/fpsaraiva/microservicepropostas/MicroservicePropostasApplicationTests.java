b91896ca09bafb72641b82e67d5aaf7be2c3b219
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-08-20 13:51:48 -0300

    [4.11.2] feat: add setup do projeto

---
+++ src/test/java/br/com/zupacademy/fpsaraiva/microservicepropostas/MicroservicePropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.fpsaraiva.microservicepropostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
