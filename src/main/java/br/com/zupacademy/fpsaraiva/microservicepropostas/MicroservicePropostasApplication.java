39eee6fcc86b741ec6bd87b85ac08d5f1a9dbec0
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-08-25 16:17:50 -0300

    [4.11.6] add dependencias feign

--- src/main/java/br/com/zupacademy/fpsaraiva/microservicepropostas/MicroservicePropostasApplication.java
+++ src/main/java/br/com/zupacademy/fpsaraiva/microservicepropostas/MicroservicePropostasApplication.java
@@ -2,8 +2,12 @@ package br.com.zupacademy.fpsaraiva.microservicepropostas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaRepositories(enableDefaultTransactions = false)
 public class MicroservicePropostasApplication {
 
 	public static void main(String[] args) {
