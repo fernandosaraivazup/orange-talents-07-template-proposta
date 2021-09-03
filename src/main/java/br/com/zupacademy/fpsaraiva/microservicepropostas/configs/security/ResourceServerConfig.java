c4291925befc7dadb63c6e32a60f034f3e9cc912
Fernando Saraiva <fernando.saraiva@zup.com.br>
2021-09-03 16:32:33 -0300

    [4.11.15] feat: Como saber se nosso sistema está funcionando corretamente

--- src/main/java/br/com/zupacademy/fpsaraiva/microservicepropostas/configs/security/ResourceServerConfig.java
+++ src/main/java/br/com/zupacademy/fpsaraiva/microservicepropostas/configs/security/ResourceServerConfig.java
@@ -18,6 +18,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.GET, "/propostas/**").hasAuthority("SCOPE_escopo-proposta")
                         .antMatchers(HttpMethod.POST, "/propostas").hasAuthority("SCOPE_escopo-proposta")
                         .antMatchers(HttpMethod.POST, "/cartoes/**").hasAuthority("SCOPE_escopo-proposta")
+                        .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                         .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
